39ee5d6366eaf4b450548ce198c1fa2ff84bcd81
Sriram Nambakam <snambakam@gmail.com>
2025-01-11 22:59:16 +0000

    Add install script for toolchain dependencies on Ubuntu 24

---
+++ support/scripts/ubuntu24-toolchain-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt install -y \
+	autoconf \
+        automake \
+        binutils \
+        bison \
+	build-essential \
+        gawk \
+        gcc \
+        krb5-multidev \
+        libtool \
+        libxml2 \
+        libxml2-dev \
+        make \
+        libncurses-dev \
+        libldap-dev \
+        libssl-dev \
+        libpopt-dev \
+        procps \
+        sed \
+        libsqlite3-dev \
+        tar
