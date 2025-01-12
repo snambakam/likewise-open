7d760548aff2f9087cfd3223448585601a9ee7e7
Sriram Nambakam <snambakam@gmail.com>
2025-01-12 00:38:24 +0000

    Add Makefile.am and configure.ac - Start creating files for AutoTool based build.

---
+++ support/make/makedefs.mk
@@ -0,0 +1,27 @@
+MKDIR=/bin/mkdir
+RM=/bin/rm
+RMDIR=/bin/rm -rf
+CP=/bin/cp
+MV=/bin/mv
+TAR=/bin/tar
+RPM=/bin/rpm
+SED=/bin/sed
+
+SRCROOT := $(realpath $(SRCROOT))
+
+LIGHTWAVE_STAGE_DIR=$(SRCROOT)/stage
+
+ARCH=x86_64
+DIST="%{nil}"
+
+LW_SERVER_MAJOR_VER=1
+LW_SERVER_MINOR_VER=3
+LW_SERVER_RELEASE_VER=1
+LW_SERVER_PATCH_VER=35
+
+JAVA_HOME?=/etc/alternatives/jre/../
+COMMONS_DAEMON?=/usr/share/java/
+ANT_HOME?=/var/opt/apache-ant
+TOMCAT_HOME?=/var/opt/apache-tomcat
+MAVEN_HOME?=/var/opt/apache-maven
+LIGHTWAVE_LOGDIR?=/var/log/lightwave
